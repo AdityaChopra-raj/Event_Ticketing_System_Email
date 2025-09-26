64431f266b8b63df55a7779353f1c4bf3ef6f55d
AdityaChopra-raj <chopraadee2@gmail.com>
2025-09-26 15:47:26 +0530

    Update events_data.py

--- events_data.py
+++ events_data.py
@@ -1,22 +1,18 @@
 events = {
     "Navratri Pooja": {
         "image": "images/navratri.jpg",
-        "capacity": 150,
-        "tickets_scanned": 0
+        "capacity": 150
     },
     "Diwali Dance": {
         "image": "images/diwali.jpg",
-        "capacity": 150,
-        "tickets_scanned": 0
+        "capacity": 150
     },
     "Freshers": {
         "image": "images/freshers.jpg",
-        "capacity": 150,
-        "tickets_scanned": 0
+        "capacity": 150
     },
     "Ravan Dehan": {
         "image": "images/ravan.jpg",
-        "capacity": 150,
-        "tickets_scanned": 0
+        "capacity": 150
     }
 }
