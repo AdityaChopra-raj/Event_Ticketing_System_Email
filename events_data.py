b0da1f4e9aedccc20115911c30a71ff301b265bf
AdityaChopra-raj <chopraadee2@gmail.com>
2025-09-26 13:04:37 +0530

    Create events_data.py

---
+++ events_data.py
@@ -0,0 +1,24 @@
+# events_data.py
+# Each event has: name, image path, capacity, tickets_scanned
+events = {
+    "Navratri Pooja": {
+        "image": "images/navratri.jpg",
+        "capacity": 150,
+        "tickets_scanned": 0
+    },
+    "Diwali Dance": {
+        "image": "images/diwali.jpg",
+        "capacity": 150,
+        "tickets_scanned": 0
+    },
+    "Freshers": {
+        "image": "images/freshers.jpg",
+        "capacity": 150,
+        "tickets_scanned": 0
+    },
+    "Ravan Dehan": {
+        "image": "images/ravan.jpg",
+        "capacity": 150,
+        "tickets_scanned": 0
+    }
+}
